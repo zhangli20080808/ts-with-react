aed7acd62611562d4db792c3b732bb321d27fc53
zhangli20080808 <791492434@qq.com>
2020-12-12 12:03:52 +0800

    ceshi

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
+import Button from './components/Button/button';
 const App: React.FC = () => {
-  return <div>123</div>;
+  return (
+    <div>
+      <Button className="customer">普通按钮</Button>
+      <Button disabled>disabled</Button>
+      <Button btnType="primary" size="lg">
+        Primary
+      </Button>
+      <Button btnType="primary" size="sm">
+        Primary
+      </Button>
+      <Button btnType="danger" size="sm">
+        Danger
+      </Button>
+      <Button btnType="link" href="www.baidu.com" target="_black">
+        Link
+      </Button>
+      <Button btnType="link" href="www.baidu.com" disabled>
+        Link
+      </Button>
+    </div>
+  );
 };
-
-
-export default  App
+ 
+export default App;
