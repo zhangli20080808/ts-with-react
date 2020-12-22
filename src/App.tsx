d74eea5f4f3629d621e551a54dd7baf3b2450f77
zhangli20080808 <791492434@qq.com>
2020-12-22 22:08:06 +0800

    zl

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,37 @@ import React from 'react';
 import Button from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
+
 const App: React.FC = () => {
-  return (
-    <div style={{padding:30}}>
-      <Menu defaultIndex='0' onSelect={(index) => alert(index)} mode='vertical'>
-        <MenuItem index='1'>1</MenuItem>
-        <MenuItem index='2' disabled>2</MenuItem>
-        <MenuItem index='3'>3</MenuItem>
-        <MenuItem index='4'>4</MenuItem>
-      </Menu>
-      <Button className="customer">普通按钮</Button>
-      <Button disabled>disabled</Button>
-      <Button btnType="primary" size="lg">
-        Primary
-      </Button>
-      <Button btnType="primary" size="sm">
-        Primary
-      </Button>
-      <Button btnType="danger" size="sm">
-        Danger
-      </Button>
-      <Button btnType="link" href="www.baidu.com" target="_black">
-        Link
-      </Button>
-      <Button btnType="link" href="www.baidu.com" disabled>
-        Link
-      </Button>
-      <div>learn react</div>
-    </div>
-  );
+    return (
+        <div style={{padding: 30}}>
+            <Menu defaultIndex={"0"} onSelect={(index) => alert(index)} mode='vertical'>
+                <MenuItem>1</MenuItem>
+                <MenuItem disabled>2</MenuItem>
+                <MenuItem>3</MenuItem>
+                <MenuItem>4</MenuItem>
+                <li>123123</li>
+            </Menu>
+            <Button className="customer">普通按钮</Button>
+            <Button disabled>disabled</Button>
+            <Button btnType="primary" size="lg">
+                Primary
+            </Button>
+            <Button btnType="primary" size="sm">
+                Primary
+            </Button>
+            <Button btnType="danger" size="sm">
+                Danger
+            </Button>
+            <Button btnType="link" href="www.baidu.com" target="_black">
+                Link
+            </Button>
+            <Button btnType="link" href="www.baidu.com" disabled>
+                Link
+            </Button>
+            <div>learn react</div>
+        </div>
+    );
 };
 
 export default App;
--- src/hooks/useMouseHooks.tsx
+++ src/hooks/useMouseHooks.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 const useMouseHooks = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  useEffect(() => {
-    const updateMoves = (e: MouseEvent) => {
-      setPosition({ x: e.clientY, y: e.clientY });
-    };
-    document.addEventListener('mousemove', updateMoves);
-    return () => {
-      document.removeEventListener('mousemove', updateMoves);
-    };
-  }, []);
+    const [position, setPosition] = useState({x: 0, y: 0});
+    useEffect(() => {
+        const updateMoves = (e: MouseEvent) => {
+            setPosition({x: e.clientX, y: e.clientY});
+        };
+        document.addEventListener('mousemove', updateMoves);
+        return () => {
+            document.removeEventListener('mousemove', updateMoves);
+        };
+    }, []);
 
-  return position;
+    return position;
 };
 
 export default useMouseHooks;
