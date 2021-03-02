993e302e5822ce05a8602a68f99e60918d48ef42
zhangli20080808 <791492434@qq.com>
2021-03-02 23:54:47 +0800

    基础结构

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,5 +1,12 @@
-import React, { FC, InputHTMLAttributes, ReactElement } from 'react';
+import React, {
+  CSSProperties,
+  FC,
+  InputHTMLAttributes,
+  ReactElement,
+} from 'react';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import classNames from 'classnames';
+import Icon from '../Icon/icon';
 
 type InputSize = 'lg' | 'sm';
 
@@ -8,14 +15,37 @@ export interface InputProps
   disabled?: boolean;
   size?: InputSize;
   icon?: IconProp;
-  prepand?: string | ReactElement;
+  style?: CSSProperties;
+  prepend?: string | ReactElement;
   append?: string | ReactElement;
 }
 
 export const Input: FC<InputProps> = (props) => {
-  const { disabled, size, icon, prepand, append } = props;
+  const { disabled, size, icon, prepend, append, style, ...restProps } = props;
   // 取出各种属性
   // 根据属性计算不同的className
+  const cNames = classNames('viking-input-wrapper', {
+    [`input-size-${size}`]: size,
+    'is-disabled': disabled,
+    'input-group': prepend || append,
+    'input-group-append': !!append,
+    'input-group-prepend': !!prepend,
+  });
   // 根据属性判断是否要添加特定的节点
-  return <React.Fragment></React.Fragment>;
+  return (
+    <div className={cNames} style={style}>
+      {prepend && <div className="viking-input-group-prepend">{prepend}</div>}
+      {icon && (
+        <div className="icon-wrapper">
+          <Icon icon={icon} title={`title-${icon}`} />
+        </div>
+      )}
+      <input
+        className="viking-input-inner"
+        disabled={disabled}
+        {...restProps}
+      />
+      {append && <div className="viking-input-group-append">{append}</div>}
+    </div>
+  );
 };
