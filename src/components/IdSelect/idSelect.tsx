95ac7253e887d45742ead893194e9e130f83fb3e
zhangli20080808 <791492434@qq.com>
2021-03-04 21:41:14 +0800

    idSelect

---
+++ src/components/IdSelect/idSelect.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type SelectProps = React.ComponentProps<typeof Select>
+
+interface IdSelectProps extends Omit<SelectProps, 'value' | 'onChange' | 'defaultOptionName' | 'options'> {
+    value: string | number | undefined | null;
+    onChange: (value?: number) => void;
+    defaultOptionName?: string;
+    options?: { value: number; label: string }[];
+}
+
+/**
+ * value可以传入多种类型的值
+ * onChange只会回调number和undefined类型
+ * 当isNaN(Number(value))为true的时候，代表选择默认类型
+ * 当选择默认类型的时候，onChange会回调 undefined
+ * @param props
+ * @constructor
+ */
+export const IdSelect = (props: IdSelectProps) => {
+    const {value, onChange, defaultOptionName, options, ...restProps} = props;
+    return (
+        <Select
+            value={toNumber(value)}
+            onChange={value => onChange(toNumber(value) || undefined)}
+            {...restProps}
+        >
+            {defaultOptionName ? <Select.Option value={0}>{defaultOptionName}</Select.Option>}
+            {
+                options?.map(option => <Select.Option key={option.value}
+                                                      value={option.value}>{option.label}</Select.Option>)
+            }
+        </Select>
+    );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
