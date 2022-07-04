17f19fb97b2b8367bc668d1de2227f3fd4ea0371
zhangli20080808 <791492434@qq.com>
2022-07-04 19:41:24 +0800

    zl

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,9 @@ import Transition from "./components/Transition/transition";
 
 import Icon from "./components/Icon/icon";
 import IdSelect from "./components/IdSelect/idSelect";
-import Upload2 from "./components/upload2";
-import { Upload } from "./components/Upload/upload";
-import Tree from "./components/Tree/index";
+// import Upload2 from "./components/upload2";
+import Upload from "./components/Upload/upload";
+// import Tree from "./components/Tree/index";
 
 library.add(fas); // 把图标添加进仓库
 
